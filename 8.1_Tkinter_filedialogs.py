eeba2fc4987f8eb01c9ab95535a4c1dadb795b05
Ritesh Kapse <68165397+RITESH-Kapse@users.noreply.github.com>
2020-10-18 01:27:43 +0530

    Add files via upload
    All the python codes with openpyxl/

---
+++ 8.1_Tkinter_filedialogs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+Open file and save file dialogs with tkinter
+Icon by Paulo Ruberto
+http://www.iconarchive.com/show/custom-round-yosemite-icons-by-pauloruberto/Python-icon.html
+"""
+import tkinter as tk
+from tkinter import filedialog
+from os import getcwd
+from openpyxl import load_workbook
+
+if __name__ == "__main__": 
+    root = tk.Tk()
+    root.withdraw() # Hides the root window
+    #root.wm_iconbitmap('py.ico')
+    
+    root.filename =  filedialog.asksaveasfilename(initialdir=getcwd(),\
+                                                  title = "Select file",\
+                                                  filetypes = (("png files","*.png"),\
+                                                               ("all files","*.*")))
+
+    print ("Save as file path:", root.filename)
+
+##    root.filename =  filedialog.askopenfilename(initialdir=getcwd(),\
+##                                                title = "Select file",\
+##                                                filetypes = (("Excel workbooks","*.xlsx"),\
+##                                                             ("all files","*.*")))
+##    print ("Open file path:", root.filename)
+##
+##    
+##    wb = load_workbook(root.filename)
+##    print("Sheets in workbook", wb.worksheets)
